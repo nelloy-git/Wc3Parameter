93c06a98eb35c97a1fbe66fcf71486028c7705d3
nelloy-git <nelloy93@yandex.ru>
2020-11-16 02:33:40 +0300

    fix

--- API.lua
+++ API.lua
@@ -14,12 +14,12 @@ LibManager.addDepency('Wc3Utils', 'https://github.com/nelloy-git/Wc3Utils.git')
 ---@class Wc3Parameter
 local Wc3Parameter = {}
 
----@type ParameterTypeClass
-local ParameterType = require('Type') or error('')
----@type ParameterValueTypeClass
-local ParameterValueType = require('ValueType') or error('')
 ---@type ParameterContainerClass
 Wc3Parameter.Container = require('Container') or error('')
+---@type ParameterTypeClass
+Wc3Parameter.Type = require('Type') or error('')
+---@type ParameterValueTypeClass
+Wc3Parameter.ValueType = require('ValueType') or error('')
 ---@type ParameterContainerUnitClass
 Wc3Parameter.UnitContainer = require('Container.Unit') or error('')
 
